6d3ebf456e9e833915320bcb42cd73e4d8455d48
ShotMeow <shotmeow@gmail.com>
2024-07-10 23:34:23 +0300

    Fixed: toaster theme

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,13 +1,10 @@
-import { useTheme } from "next-themes";
 import { Toaster as Sonner } from "sonner";
 import type { ComponentProps } from "react";
 
 type ToasterProps = ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
-  return <Sonner theme={theme as ToasterProps["theme"]} {...props} />;
+  return <Sonner theme="dark" {...props} />;
 };
 
 export { Toaster };
